5b59e9787e2d23f8c52c4751015d7baa63a82ed3
crasher746 <Crasher746@gmail.com>
2017-04-19 00:52:47 +0700

    Edit_6

---
+++ test/1_deposit_test.c
@@ -0,0 +1,44 @@
+#include "ctest.h"
+#include "deposit.h"
+
+CTEST (CorrectnessOfDataEntry, WrongTermWrongContrib)
+{
+	const int Input_Data = Input (-1, 900);
+	const int Expected_Data = 1;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
+
+CTEST (CorrectnessOfDataEntry, WrongTerm1WrongContrib1)
+{
+	const int Input_Data = Input (366, 900);
+	const int Expected_Data = 1;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
+
+CTEST (CorrectnessOfDataEntry, WrongTermTrueContrib)
+{
+	const int Input_Data = Input (-1, 1500);
+	const int Expected_Data = 1;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
+
+CTEST (CorrectnessOfDataEntry, WrongTerm1TrueContrib)
+{
+	const int Input_Data = Input (366, 1500);
+	const int Expected_Data = 1;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
+
+CTEST (CorrectnessOfDataEntry, TrueTermWrongContrib)
+{
+	const int Input_Data = Input (100, -1000);
+	const int Expected_Data = 1;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
+
+CTEST (CorrectnessOfDataEntry, TrueTermTrueContrib)
+{
+	const int Input_Data = Input (200, 2000);
+	const int Expected_Data = 0;
+	ASSERT_EQUAL(Expected_Data, Input_Data);
+}
