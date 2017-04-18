d55a04f4a6378a24205d8e4c1904d36359b93cbb
crasher746 <Crasher746@gmail.com>
2017-04-19 00:49:50 +0700

    Edit

---
+++ src/deposit.c
@@ -0,0 +1,26 @@
+//Realise functions
+#include <stdio.h>
+#include "deposit.h"
+
+int Input (int term, int contrib) {
+	if (term < 0 || term > 365 || contrib < 1000) {
+		return 1;
+	}
+	return 0;
+}
+
+int Process (int term, int contrib) {
+	if (term >= 0 && term <= 30) {
+		contrib *= 0.9;
+	}
+	if (term >= 31 && term <= 120) {
+		contrib *= 1.02;
+	}
+	if (term >= 121 && term <= 240) {
+		contrib *= 1.06;
+	}
+	if (term >= 241 && term <= 365) {
+		contrib *= 1.12;
+	}
+	return contrib;
+}
